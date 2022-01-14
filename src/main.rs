0abeeb3ebb37b6af6816f0044fe80d89c19c816b
moonbench <moonbench@users.noreply.github.com>
2022-01-14 19:49:38 +1100

    Switch to progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 extern crate rand;
 
-mod tui;
-
 use rand::thread_rng;
 use rand::Rng;
 use std::fs;
 use std::fs::{File};
 use std::io::{Write};
+use termprogress::prelude::*;
 
 const PAD_DIR: &str = "pads";
 
@@ -27,7 +26,7 @@ fn generate_pads(pad_count: &str, pad_size: &str) {
     let pad_size: i32 = pad_size.parse().expect("[ ERROR ] Invalid pad size");
     fs::create_dir_all(PAD_DIR).expect(&format!("[ ERROR ] Unable to make directory {}", PAD_DIR).to_string());
 
-    info!(format!("Generating {} pads with {} bytes each", pad_count, pad_size));
+    println!("Generating {} one-time pads with {} bytes each", pad_count, pad_size);
 
     for pad_number in 0..pad_count {
         generate_pad(pad_number, pad_size);
@@ -35,18 +34,25 @@ fn generate_pads(pad_count: &str, pad_size: &str) {
 }
 
 fn generate_pad(pad_id: i32, size: i32) {
-    status!(format!("Generating {}/pad{}.pad", PAD_DIR, pad_id));
+    let mut progress = Bar::default();
+    progress.set_title(&format!("Generating pad {}...", pad_id).to_string());
 
     let mut rng = thread_rng();
     let mut bytes: Vec<u8> = Vec::new();
-    for _ in 0..size {
+    for i in 0..size {
         bytes.push(rng.gen());
+        if i % 1000 == 0 {
+            progress.set_progress(i as f64/size as f64);
+        }
     }
+
+    progress.set_progress(1.0);
+    progress.set_title(&format!("Generated pad {}.", pad_id).to_string());
 
     let output_path = format!("{}/pad{}.pad", PAD_DIR, pad_id).to_string();
     let mut file = File::create(&output_path).expect("[ ERROR ] Failed to create file");
     file.write_all(&bytes).expect("[ ERROR ] Failed to write to file");
-    success!("Done.");
+    progress.complete();
 }
 
 fn main() {
